f8a8edeb5cd94d7bf8bedd43f2bfb9d429c15626
Ronald <u201920185@upc.edu.pe>
2023-11-22 00:52:07 -0500

    sss

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+
+
+
+@NgModule({
+    declarations: [],
+    imports: [],
+    exports: [],
+    providers: [],
+    bootstrap: []
+
+})
+
+export class AppRoutingModule { }
+export class YourModule { }
